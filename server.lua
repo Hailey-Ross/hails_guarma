08a7577b06304051ee9079071b4b803104aac3fa
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-13 21:26:10 -0600

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,11 +4,17 @@ TriggerEvent("getCore",function(core)
     VorpCore = core
 end)
 
+--[[ TODO: Make it so you can't go into a negative balance from either transaction ]]
 RegisterServerEvent('wcrp:guarmaticketbuy')
 AddEventHandler("wcrp:guarmaticketbuy", function()
     local User = VorpCore.getUser(source)
     local _source = source
     local Character = User.getUsedCharacter
+    if Character.money < Config.PricetoGuarma then
+        TriggerClientEvent("vorp:TipBottom", _source, Config.NoMoney, 4000)
+        return
+    end
+
     Character.removeCurrency(0, Config.PricetoGuarma)
 end)
 
@@ -17,5 +23,10 @@ AddEventHandler("wcrp:guarmaticketbuy2", function()
     local User = VorpCore.getUser(source)
     local _source = source
     local Character = User.getUsedCharacter
+    if Character.money < Config.PricetoSD then
+        TriggerClientEvent("vorp:TipBottom", _source, Config.NoMoney, 4000)
+        return
+    end
+
     Character.removeCurrency(0, Config.PricetoSD)
 end)
