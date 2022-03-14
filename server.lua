200972d3505597ac82567253f9292b009abdc3da
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-13 21:15:03 -0600

    Update server.lua

--- server.lua
+++ server.lua
@@ -6,20 +6,16 @@ end)
 
 RegisterServerEvent('wcrp:guarmaticketbuy')
 AddEventHandler("wcrp:guarmaticketbuy", function()
-    local _source = source
     local User = VorpCore.getUser(source)
+    local _source = source
     local Character = User.getUsedCharacter
     Character.removeCurrency(0, Config.PricetoGuarma)
-    TriggerClientEvent("vorp:TipBottom", _source, Config.TravelTiptextTO, 5000)
-        
 end)
 
 RegisterServerEvent('wcrp:guarmaticketbuy2')
 AddEventHandler("wcrp:guarmaticketbuy2", function()
-    local _source = source
     local User = VorpCore.getUser(source)
+    local _source = source
     local Character = User.getUsedCharacter
     Character.removeCurrency(0, Config.PricetoSD)
-    TriggerClientEvent("vorp:TipBottom", _source, Config.TravelTiptextFROM, 5000)
-        
 end)
