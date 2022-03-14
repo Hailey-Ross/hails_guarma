8c449a6109f585c44348e55fd3e50652b9a6bb4f
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-13 21:14:26 -0600

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,14 +1,14 @@
-
-
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(1)
 	local coords = GetEntityCoords(PlayerPedId())
-	if (Vdist(coords.x, coords.y, coords.z, 2671.13, -1552.96, 46.47) < 2.0) then
-            DrawTxt(Config.TraveltextTOGuarma, 0.50, 0.85, 0.7, 0.7, true, 255, 255, 255, 255, true)
+        local traveltext = Config.TraveltextTOGuarma
+        if (Vdist(coords.x, coords.y, coords.z, 2671.13, -1552.96, 46.47) < 2.0) then
+            DrawTxt( traveltext, 0.50, 0.85, 0.7, 0.7, true, 255, 255, 255, 255, true)
             if IsControlJustReleased(0, 0x760A9C6F) then -- g
-            local player = PlayerPedId()
+                local player = PlayerPedId()
                 TriggerServerEvent('wcrp:guarmaticketbuy')
+                TriggerEvent("vorp:TipBottom", Config.TravelTiptextTO, 5000)
                 FreezeEntityPosition(PlayerPedId(),true)
 		Wait(5000)
                 DoScreenFadeOut(5000)
@@ -30,10 +30,11 @@ Citizen.CreateThread(function()
 		Citizen.Wait(1)
 	local coords = GetEntityCoords(PlayerPedId())
 	if (Vdist(coords.x, coords.y, coords.z, 1270.0314, -6854.6113, 43.4185) < 2.0) then
-            DrawTxt(Config.TraveltextTOSD, 0.50, 0.85, 0.7, 0.7, true, 255, 255, 255, 255, true)
-            if IsControlJustReleased(0, 0x760A9C6F) then
-                local player = PlayerPedId()
+        	DrawTxt( Config.TraveltextTOSD, 0.50, 0.85, 0.7, 0.7, true, 255, 255, 255, 255, true)
+	if IsControlJustReleased(0, 0x760A9C6F) then
+	local player = PlayerPedId()
                 TriggerServerEvent('wcrp:guarmaticketbuy2')
+                TriggerEvent("vorp:TipBottom", Config.TravelTiptextFROM, 5000)
 		FreezeEntityPosition(PlayerPedId(),true)
 		DoScreenFadeOut(5000)
                 Wait(5000)
@@ -52,11 +53,11 @@ end)
 
 
 function DrawTxt(str, x, y, w, h, enableShadow, col1, col2, col3, a, centre)
-    local str = CreateVarString(10, "LITERAL_STRING", str, Citizen.ResultAsLong())
-   SetTextScale(w, h)
-   SetTextColor(math.floor(col1), math.floor(col2), math.floor(col3), math.floor(a))
-   SetTextCentre(centre)
-   if enableShadow then SetTextDropshadow(1, 0, 0, 0, 255) end
-   Citizen.InvokeNative(0xADA9255D, 10);
-   DisplayText(str, x, y)
-end
+	local str = CreateVarString(10, "LITERAL_STRING", str, Citizen.ResultAsLong())
+		SetTextScale(w, h)
+		SetTextColor(math.floor(col1), math.floor(col2), math.floor(col3), math.floor(a))
+		SetTextCentre(centre)
+	if enableShadow then SetTextDropshadow(1, 0, 0, 0, 255) end
+		Citizen.InvokeNative(0xADA9255D, 10);
+		DisplayText(str, x, y)
+	end
