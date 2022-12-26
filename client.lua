8c72b0f9bc6ec14b353e749987744edab5b236ea
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-12-26 11:00:04 -0700

    Code formatting / cleanup

--- client.lua
+++ client.lua
@@ -1,12 +1,13 @@
-local transitions = Config.transitions   --Transition Config
-local Debug = Config.Debug               --Debug Flag
-local Floor = Config.SeedFloor           --Set max floor for RNG
-local Ceiling = Config.SeedCeiling       --Set max ceiling for RNG
-local MasterCeiling = Ceiling + Ceiling  --Create maximum of ceiling from math(s)
+local transitions = Config.transitions     --Transition Config
+local Debug = Config.Debug                 --Debug Flag
+local Floor = Config.SeedFloor             --Set max floor for RNG
+local Ceiling = Config.SeedCeiling         --Set max ceiling for RNG
+local MasterCeiling = Ceiling + Ceiling    --Create maximum of ceiling from math(s)
 local testsuccess, result = pcall(os.time) --Time Module Test Call
 local ctestsuccess, crypto = pcall(require, "crypto") --Crypto Module Test Call
 local seed = math.random(Floor,Ceiling) + math.random(Floor,Ceiling) * math.random(1,2) - math.random(-MasterCeiling,MasterCeiling) -- Default Seed Generation ALL BUILT IN LUA
-local time --set Time Variable
+local time                                 --set Time Variable
+local traveltext                           --set traveltext variable
 
 if ctestsuccess and testsuccess then --IF we have both required modules pass their tests, use both for Seed Generation
 	time = os.time() % 10000
@@ -31,18 +32,18 @@ Citizen.CreateThread(function()
 	while true do
 	Citizen.Wait(1)
 	local coords = GetEntityCoords(PlayerPedId())
-        local traveltext = Config.TraveltextTOGuarma
-        local diceroll = math.random(1,12)
-        local diceroll2 = math.random(1,12)
-        local diceroll3 = math.random(1,12)
-        local diceroll4 = math.random(1,12)
-        local diceroll5 = math.random(1,12)
-        local spawnroll = diceroll + diceroll2 + diceroll3 + diceroll4 + diceroll5
+    traveltext = Config.TraveltextTOGuarma
         if (Vdist(coords.x, coords.y, coords.z, 2671.13, -1552.96, 46.47) < 2.0) then
             DrawTxt( traveltext, 0.50, 0.85, 0.7, 0.5, true, 255, 255, 255, 255, true)
             if IsControlJustReleased(0, 0x760A9C6F) then
                 local player = PlayerPedId()
                 local Ped = PlayerPedId()
+                local diceroll = math.random(1,12)
+                local diceroll2 = math.random(1,12)
+                local diceroll3 = math.random(1,12)
+                local diceroll4 = math.random(1,12)
+                local diceroll5 = math.random(1,12)
+                local spawnroll = diceroll + diceroll2 + diceroll3 + diceroll4 + diceroll5
                 if Debug == true then print("Pre-roll 1: "..diceroll); print("Pre-roll 2: "..diceroll2); print("Pre-roll 3: "..diceroll3); print("Pre-roll 4: "..diceroll4); print("Pre-roll 5: "..diceroll5); print("Roll Math: "..diceroll.." + "..diceroll2.." + "..diceroll3.." + "..diceroll4.." + "..diceroll5) end
                 TriggerServerEvent('wcrp:guarmaticketbuy')
                 TriggerEvent("vorp:TipBottom", Config.TravelTiptextTO, 5000)
@@ -86,23 +87,24 @@ Citizen.CreateThread(function() -- TP Back from Guarma
 	while true do
 	Citizen.Wait(1)
 	local coords = GetEntityCoords(PlayerPedId())
-        local diceroll = math.random(1,5)
-        local diceroll2 = math.random(1,5)
-        local diceroll3 = math.random(1,5)
-        local diceroll4 = math.random(1,5)
-        local diceroll5 = math.random(1,5)
-        local spawnroll = diceroll + diceroll2 + diceroll3 + diceroll4 + diceroll5
+    traveltext = Config.TraveltextTOSD
     if (Vdist(coords.x, coords.y, coords.z, 1265.8421, -6852.1635, 43.4185) < 2.0) then
-            DrawTxt( Config.TraveltextTOSD, 0.50, 0.85, 0.7, 0.5, true, 255, 255, 255, 255, true)
+            DrawTxt( traveltext, 0.50, 0.85, 0.7, 0.5, true, 255, 255, 255, 255, true)
             if IsControlJustReleased(0, 0x760A9C6F) then
                 local player = PlayerPedId()
                 local Ped = PlayerPedId()
+                local diceroll = math.random(1,12)
+                local diceroll2 = math.random(1,12)
+                local diceroll3 = math.random(1,12)
+                local diceroll4 = math.random(1,12)
+                local diceroll5 = math.random(1,12)
+                local spawnroll = diceroll + diceroll2 + diceroll3 + diceroll4 + diceroll5
                 if Debug == true then print("Pre-roll 1: "..diceroll); print("Pre-roll 2: "..diceroll2); print("Pre-roll 3: "..diceroll3); print("Pre-roll 4: "..diceroll4); print("Pre-roll 5: "..diceroll5); print("Roll Math: "..diceroll.." + "..diceroll2.." + "..diceroll3.." + "..diceroll4.." + "..diceroll5) end
                 TriggerServerEvent('wcrp:guarmaticketbuy2')
                 TriggerEvent("vorp:TipBottom", Config.TravelTiptextFROM, 5000)
                 if transitions == true then ExecuteCommand('hud') end
                 if transitions == true then DoScreenFadeOut(3000) end
-		FreezeEntityPosition(Ped,true)
+		        FreezeEntityPosition(Ped,true)
                 Wait(3000)
                 if transitions == true then DoScreenFadeIn(500); Citizen.InvokeNative(0x1E5B70E53DB661E5, 0, 0, 0, 'Mainland', 'Saint Denis Harbor', 'Feeling refreshed. .') end
                 Citizen.InvokeNative(0x74E2261D2A66849A, 0)
@@ -111,30 +113,30 @@ Citizen.CreateThread(function() -- TP Back from Guarma
                 SetEntityCoords(Ped, 2534.975, -1835.944, 41.93) -- Boat Transition Area
                 Wait(20000)
                 --Wait(2000)
-                if spawnroll == 5 then
-                    if Debug == true then print("Section: 5"); print("Final Roll: "..spawnroll) end
+                if spawnroll <= 5 and spawnroll >= 15 then
+                    if Debug == true then print("Section: 5 - 15"); print("Final Roll: "..spawnroll) end
                     SetEntityCoords(Ped, 2670.69, -1547.34, 45.819)
-                elseif spawnroll >= 6 and spawnroll <= 9 then
-                    if Debug == true then print("Section: 6-9"); print("Final Roll: "..spawnroll) end
+                elseif spawnroll <= 16 and spawnroll >= 26 then
+                    if Debug == true then print("Section: 16 - 26"); print("Final Roll: "..spawnroll) end
                     SetEntityCoords(Ped, 2673.952, -1547.87, 45.819)
-                elseif spawnroll >= 10 and spawnroll <= 15 then
-                    if Debug == true then print("Section: 10-15"); print("Final Roll: "..spawnroll) end
+                elseif spawnroll <= 27 and spawnroll >= 37 then
+                    if Debug == true then print("Section: 27 - 37"); print("Final Roll: "..spawnroll) end
                     SetEntityCoords(Ped, 2670.850, -1550.013, 45.819)
-                elseif spawnroll >= 16 and spawnroll <= 20 then
-                    if Debug == true then print("Section: 16-20"); print("Final Roll: "..spawnroll) end
+                elseif spawnroll <= 38 and spawnroll >= 48 then
+                    if Debug == true then print("Section: 38 - 48"); print("Final Roll: "..spawnroll) end
                     SetEntityCoords(Ped, 2668.939, -1545.1269, 45.819)
-                elseif spawnroll >= 21 and spawnroll <= 25 then
-                    if Debug == true then print("Section: 21-25"); print("Final Roll: "..spawnroll) end
+                elseif spawnroll <= 49 and spawnroll >= 56 then
+                    if Debug == true then print("Section: 49 - 56"); print("Final Roll: "..spawnroll) end
                     SetEntityCoords(Ped, 2674.150, -1544.462, 45.819)
                 else
-                    if Debug == true then print("Error: No Category within range"); print("Final Roll: "..spawnroll) end
+                    if Debug == true then print("Error: No Section within range"); print("Final Roll: "..spawnroll) end
                     SetEntityCoords(Ped, 2665.937, -1554.36, 45.522)
                 end
                 if transitions == true then DoScreenFadeIn(4000); ShutdownLoadingScreen() end
                 FreezeEntityPosition(Ped,false)
                 Wait(1000)
                 if transitions == true then ExecuteCommand('hud') end
-		guarma = false
+		        guarma = false
             end
         end
     end
